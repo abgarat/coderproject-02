ef0b9d438a4c813504eb97007a04ac1b081ef501
Abril Garat <abgarat@gmail.com>
2023-05-15 18:05:54 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// INGRESAR PRODUCTOS
+function agregarProductos(cantidad){
+    for(let i = cantidad; i > 0; i--){
+        let productoNombre = prompt('Ingrese el nombre del producto:');
+        let productoPrecio = Number(prompt('Ingrese el precio del producto:'));
+        while(isNaN(productoPrecio)){
+            productoPrecio = Number(prompt('Ingrese el precio del producto en números:'));
+        }
+        console.log('Usted ingresó ' + productoNombre + 'con un valor de $' + productoPrecio + 'por unidad.')
+    }
+}
+
+function definirCantProd(){
+    let n = Number(prompt('Ingrese la cantidad de productos que desea agregar al sitio:'));
+    while(isNaN(n)){
+        alert('Ingrese la cantidad escrita en números.');
+        n = Number(prompt('Ingrese la cantidad de productos que desea agregar al sitio:'));
+    }
+    return n;
+}
+
+let cantidadProductos = definirCantProd();
+
+agregarProductos(cantidadProductos);
